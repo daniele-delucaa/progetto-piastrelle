ed252d0f71e6cef9fc5f091a86bc338f6e2c3f03
Daniele <daniele.2002.ddl@gmail.com>
2024-05-24 11:09:19 +0200

    definizione tipi

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+type piastrella struct {
+	x, y int
+}
+
+type colore struct {
+	colore string
+}
+
+type piano struct {
+	piastrelle map[piastrella]colore
+	//regole []
+}
+
+func main() {
+
+}
+
+func esegui(p piano, s string) {
+
+}
